5b40aa5bd069dd59948f0727f512a51573fa2810
venu2330 <mvgr982317@gmail.com>
2023-09-25 19:14:43 +0530

    Ganesh

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args)
     {
         System.out.println("Hello Venu Gopal Reddy M");
+        System.out.println("Hello Ganesh");
     }
 }
